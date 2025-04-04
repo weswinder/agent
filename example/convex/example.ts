b302fc3154eaacb7394757798e520041ce3a8475
Ian Macartney <ianhmacartney@gmail.com>
2025-04-04 15:42:41 -0700

    example

--- example/convex/example.ts
+++ example/convex/example.ts
@@ -1,7 +1,26 @@
-import { internalMutation, query, mutation } from "./_generated/server";
+import { action } from "./_generated/server";
 import { components } from "./_generated/api";
 import { ConvexAI } from "@convex-dev/agent";
+import { generateText } from "ai";
+import { v } from "convex/values";
+import { openai } from "@ai-sdk/openai";
 
 const agent = new ConvexAI(components.agent, {
   embedder: "test",
+});
+
+const model = openai.chat("gpt-4o-mini");
+
+export const generate = action({
+  args: {
+    prompt: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const result = await generateText({
+      model,
+      prompt: args.prompt,
+    });
+    console.log(result.steps);
+    return result.text;
+  },
 });
