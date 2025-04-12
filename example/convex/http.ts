679ad49383d08b9bd04aaee7f5d6594b6c37a541
Ian Macartney <ianhmacartney@gmail.com>
2025-04-11 20:08:09 -0700

    cors for http

--- example/convex/http.ts
+++ example/convex/http.ts
@@ -1,9 +1,15 @@
 import { httpRouter } from "convex/server";
 import { streamHttpAction } from "./example";
+import { corsRouter } from "convex-helpers/server/cors";
 
 const http = httpRouter();
 
-http.route({
+const cors = corsRouter(http, {
+  allowCredentials: true,
+  allowedHeaders: ["Authorization", "Content-Type"],
+});
+
+cors.route({
   path: "/streamText",
   method: "POST",
   handler: streamHttpAction,
