3e49a7069be067b8802cbfd8aca62dbd2709b065
Ian Macartney <ianhmacartney@gmail.com>
2025-05-05 00:40:00 -0700

    improve weather

--- example/convex/weather.ts
+++ example/convex/weather.ts
@@ -45,15 +45,87 @@ export const getWeather = tool({
       current: {
         time: string;
         temperature_2m: number;
+        apparent_temperature: number;
         wind_speed_10m: number;
         wind_gusts_10m: number;
+        weather_code: number;
       };
     };
     console.log("got weather for location", data);
     return {
-      temperature: data.current.temperature_2m,
-      windSpeed: data.current.wind_speed_10m,
-      windGust: data.current.wind_gusts_10m,
+      temperature: `${(data.current.temperature_2m * 9) / 5 + 32}°F`,
+      feelsLike: `${(data.current.apparent_temperature * 9) / 5 + 32}°F`,
+      windSpeed: `${data.current.wind_speed_10m * 2.23694} mph`,
+      windGust: `${data.current.wind_gusts_10m * 2.23694} mph`,
+      description: nameOfWeatherCode(data.current.weather_code),
     };
   },
 });
+
+/**
+ * Weather from https://open-meteo.com/en/docs?hourly=temperature_2m,weather_code
+ * @param code WMO code
+ * @returns text description of the weather
+ */
+function nameOfWeatherCode(code: number) {
+  switch (code) {
+    case 0:
+      return "Clear";
+    case 1:
+      return "Mainly clear";
+    case 2:
+      return "Partly cloudy";
+    case 3:
+      return "Overcast";
+    case 45:
+      return "Fog and depositing rime fog";
+    case 48:
+      return "Fog and depositing rime fog";
+    case 51:
+      return "Drizzle: Light";
+    case 53:
+      return "Drizzle: Moderate";
+    case 55:
+      return "Drizzle: Dense intensity";
+    case 56:
+      return "Freezing Drizzle: Light and dense intensity";
+    case 57:
+      return "Freezing Drizzle: Dense intensity";
+    case 61:
+      return "Light Rain";
+    case 63:
+      return "Moderate Rain";
+    case 65:
+      return "Heavy Rain";
+    case 66:
+      return "Light Freezing Rain";
+    case 67:
+      return "Heavy Freezing Rain";
+    case 71:
+      return "Lightly Snow";
+    case 73:
+      return "Snowing";
+    case 75:
+      return "Snowing heavily";
+    case 77:
+      return "Snow grains";
+    case 80:
+      return "Rain showers: Slight";
+    case 81:
+      return "Rain showers: Moderate";
+    case 82:
+      return "Rain showers: Violent";
+    case 85:
+      return "Snow showers: Slight";
+    case 86:
+      return "Snow showers: Heavy";
+    case 95:
+      return "Thunderstorm";
+    case 96:
+      return "Thunderstorm with light hail";
+    case 99:
+      return "Thunderstorm with heavy hail";
+    default:
+      return "Unknown";
+  }
+}
