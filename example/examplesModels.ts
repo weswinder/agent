b339b08c311c324bf538d311c3dfa6639001ff76
Ian Macartney <ianhmacartney@gmail.com>
2025-06-07 23:06:37 -0700

    use openrouter if specified

--- example/examplesModels.ts
+++ example/examplesModels.ts
@@ -1,13 +1,25 @@
+import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 import { openai } from "@ai-sdk/openai";
 
-if (!process.env.OPENAI_API_KEY) {
+if (!process.env.OPENAI_API_KEY && !process.env.OPENROUTER_API_KEY) {
   throw new Error(
-    "Run `npx convex env set OPENAI_API_KEY=<your-api-key>` from the example directory to set the API key.",
+    "Run `npx convex env set OPENAI_API_KEY=<your-api-key>` or `npx convex env set OPENROUTER_API_KEY=<your-api-key>` from the example directory to set the API key.",
   );
 }
 
+let chat, textEmbedding;
+
+if (process.env.OPENAI_API_KEY) {
+  chat = openai.chat("gpt-4o-mini");
+  textEmbedding = openai.textEmbeddingModel("text-embedding-3-small");
+}
+
+if (process.env.OPENROUTER_API_KEY) {
+  const openrouter = createOpenRouter({
+    apiKey: process.env.OPENROUTER_API_KEY,
+  });
+  chat = openrouter.chat("openai/gpt-4o-mini");
+}
+
 // If you want to use different models for examples, you can change them here.
-export const chat = openai.chat("gpt-4o-mini");
-export const textEmbedding = openai.textEmbeddingModel(
-  "text-embedding-3-small",
-);
+export { chat, textEmbedding };
