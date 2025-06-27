860900fcd492920a0cf22b99aa193f786daad962
Ian Macartney <ianhmacartney@gmail.com>
2025-06-27 01:04:03 -0700

    snipcommonjs

--- eslint.config.js
+++ eslint.config.js
@@ -7,13 +7,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 export default [
   { files: ["src/**/*.{js,mjs,cjs,ts,tsx}"] },
   {
-    ignores: [
-      "dist/**",
-      "eslint.config.js",
-      "setup.cjs",
-      "**/_generated/",
-      "node10stubs.mjs",
-    ],
+    ignores: ["dist/**", "eslint.config.js", "setup.cjs", "**/_generated/"],
   },
   {
     languageOptions: {
